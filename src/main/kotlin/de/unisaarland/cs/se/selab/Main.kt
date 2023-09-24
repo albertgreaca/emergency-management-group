0acd68e837a1a9f56838f647adf5d3a788ac495d
Niklas Scholz <nisc00019@stud.uni-saarland.de>
2023-09-24 14:29:41 +0200

    Started Main Function

--- src/main/kotlin/de/unisaarland/cs/se/selab/Main.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Main.kt
@@ -1,8 +1,26 @@
 package de.unisaarland.cs.se.selab
 
+import java.io.File
+
 /**
  * This is the entry point of the simulation.
  */
 fun main(args: Array<String>) {
-    throw UnsupportedOperationException()
+    val argsMap = args.toList().chunked(2).associate { it[0] to it[1] }
+    var map = argsMap["--map"] ?: return
+    var assets = argsMap["--assets"] ?: return
+    var scenario = argsMap["--scenario"] ?: return
+    var ticks = argsMap["--ticks"]?.toIntOrNull()
+    var out = argsMap["--out"]
+    if(out != null) {
+        Logger.setPrintWriter(File(out).outputStream())
+    } else {
+        Logger.setPrintWriter(System.out)
+    }
+    var f1 = File(map)
+    var f2 = File(assets)
+    var f3  = File(scenario)
+
+
+
 }
