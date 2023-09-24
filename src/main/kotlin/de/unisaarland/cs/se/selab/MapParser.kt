0f86ec3bb5a610efff211f77f6e39385b8eb7d32
Niklas Scholz <nisc00019@stud.uni-saarland.de>
2023-09-24 11:02:34 +0200

    Map Parser

--- src/main/kotlin/de/unisaarland/cs/se/selab/MapParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/MapParser.kt
@@ -16,6 +16,7 @@ class MapParser( private val gm:GraphMap, private val file:File) {
         }
         skipSpaces(true)
         val name = getNextWord(true)
+        validateId(name)
         skipSpaces(false)
         if(chars[charcounter] != '{') {
             return false
@@ -24,12 +25,21 @@ class MapParser( private val gm:GraphMap, private val file:File) {
         validateGraphMap()
         return res
     }
+    /**
+     * Parsing everything in between { }
+     * @return Parsing Successful or not
+     */
     private fun parseSgtStmtList(): Boolean {
         skipSpaces(false);
         val res1 = parseVertices()
         val res2 = parseEdges()
         return res1 && res2
     }
+    /**
+     * Parsing every Vertex of the Form
+     * Id;
+     * @return Parsing Successful or not
+     */
     private fun parseVertices(): Boolean {
         var sep = getNextSeperator()
         while(sep == ';') {
@@ -45,18 +55,79 @@ class MapParser( private val gm:GraphMap, private val file:File) {
         }
         return sep == '-'
     }
+    /**
+     * Parsing every Edge of the Form
+     * Id -> Id['attributes'];
+     * @return Parsing Successful or not
+     */
     private fun parseEdges(): Boolean {
         var sep = getNextSeperator()
         while(sep == '-') {
-
+            val start = getNextWord(true)
+            skipSpaces(false)
+            if(chars[0] != '-' || chars[1] != '>') {
+                return false
+            }
+            charcounter+= 2;
+            val end = getNextWord(true)
+            val road = Road()
+            gm.addRoad(road, start, end)
+            parseAttributes(road)
+            if(chars[0] != ';') {
+                return false;
+            }
+            sep = getNextSeperator()
         }
+        return true
     }
-    private fun parseAttributes(): Boolean {
+    /**
+     * Parsing Attributes of the edge
+     * ['village = Id; name = Id; heightlimit = Id; name = Id; primaryType = <PrimaryType>;
+     * secondaryType = <SecondaryType>']
+     * @return Parsing Successful or not
+     */
+    private fun parseAttributes(road: Road): Boolean {
+        skipSpaces(false)
+        if(chars[0] != '[') {
+            return false
+        }
+        charcounter++
+        if(getNextWord(true) != "village") {
+            return false
+        }
+        skipSpaces(false)
+        if(chars[0] != '=') {
+            return false
+        }
+        skipSpaces(false)
+        val village = getNextWord(true)
+        validateId(village)
+        skipSpaces(false)
+        if(chars[0] != ';') {
+            return false
+        }
+        if(getNextWord(true) != "name") {
+            return false;
+        }
+        skipSpaces(false)
 
         return true
     }
 
-    private fun validateVertex(Vertex v): Boolean {
+    private fun validateId(id: String): Boolean {
+        val arr = id.toCharArray()
+        if((arr[0] < 'A' || arr[0] > 'Z') && (arr[0] < 'a' || arr[0] > 'z')) {
+            return false
+        }
+
+        for(ch in arr) {
+            if((ch < 'A' || ch > 'Z') && (ch < 'a' || ch > 'z') && ch != '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+    private fun validateVertex(v: Vertex): Boolean {
 
 
     }
@@ -76,7 +147,9 @@ class MapParser( private val gm:GraphMap, private val file:File) {
         var res = ""
         while(chars[charcounter] != ' ' || chars[charcounter] != '{'  || chars[charcounter] != '}'
             || chars[charcounter] != '[' || chars[charcounter] != ']' || chars[charcounter] != ',' ||
-            chars[charcounter] != '-' || chars[charcounter] != '>' ) {
+            chars[charcounter] != '-' || chars[charcounter] != '>' || chars[charcounter] != '='
+            || chars[charcounter] != '\n' || chars[charcounter] != '\t' || chars[charcounter] != '\u000c'
+            || chars[charcounter] != '\r' || chars[charcounter] != '\u00A0' || chars[charcounter] != '\u240b') {
 
             res += chars[charcounter]
             if(count) {
@@ -95,12 +168,14 @@ class MapParser( private val gm:GraphMap, private val file:File) {
 
     private fun skipSpaces(spaceNec: Boolean): Boolean {
         if(spaceNec) {
-            if(chars[charcounter] != ' ') {
+            if(chars[charcounter] != ' ' ||chars[charcounter] != '\n' || chars[charcounter] != '\t' || chars[charcounter] != '\u000c'
+                || chars[charcounter] != '\r' || chars[charcounter] != '\u00A0' || chars[charcounter] != '\u240b') {
                 charcounter++;
                 return false
             }
         }
-        while(chars[charcounter] == ' ') {
+        while(chars[charcounter] != ' ' ||chars[charcounter] != '\n' || chars[charcounter] != '\t' || chars[charcounter] != '\u000c'
+            || chars[charcounter] != '\r' || chars[charcounter] != '\u00A0' || chars[charcounter] != '\u240b') {
             charcounter++
         }
         return true
