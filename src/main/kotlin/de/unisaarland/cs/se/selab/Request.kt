0f9a1b5fc4ed4ece103fcd46aaeab2022dbea7af
Lara Hombrecher <laho00003@stud.uni-saarland.de>
2023-09-25 19:28:38 +0200

    documented Request class

--- src/main/kotlin/de/unisaarland/cs/se/selab/Request.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Request.kt
@@ -1,25 +1,44 @@
 package de.unisaarland.cs.se.selab
 
-class Request(private val id: Int, private val emergency: Emergency,
-              private val processingBase: Base
+/**
+ * Class to save requests
+ */
+class Request(
+    private val id: Int,
+    private val emergency: Emergency,
+    private val processingBase: Base
 ) {
-
+    /**
+     * @return Request ID
+     */
     fun getId(): Int {
         return id
     }
 
+    /**
+     * @return Emergency the request belongs to
+     */
     fun getEmergency(): Emergency {
         return emergency
     }
 
+    /**
+     * @return the Resources that are being requested
+     */
     fun getResources(): Resource {
         return emergency.getResources()
     }
 
+    /**
+     * @return Base which processes request atm
+     */
     fun getProcessingBase(): Base {
         return processingBase
     }
 
+    /**
+     * @return Base which requested (responsible base for emergency)
+     */
     fun getRequestingBase(): Base {
         return emergency.getBase()!!
     }
