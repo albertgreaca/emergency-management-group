7fd7e52a78e8c98bc184bbd54250adcac483456e
Sude Ergün <suer00002@stud.uni-saarland.de>
2023-09-25 13:31:40 +0200

    documentation vehicles

--- src/main/kotlin/de/unisaarland/cs/se/selab/Vehicle.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Vehicle.kt
@@ -1,5 +1,7 @@
 package de.unisaarland.cs.se.selab
 
+/** Class for all Vehicles
+ */
 open class Vehicle(
     private val id: Int,
     private val vehicleType: VehicleType,
@@ -12,54 +14,104 @@ open class Vehicle(
     private var available: Boolean = true
     private var baseWaitingTicks: Int = 0
     private var targetEmergency: Emergency? = null
+
+    /**
+     * getter for the id
+     * @return id of the vehicle
+     */
     fun getId(): Int {
         return id
     }
 
+    /**
+     * getter for the base
+     * @return base of the vehicle
+     */
     fun getBase(): Base {
         return base
     }
 
+    /**
+     * getter for the VehicleType
+     * @return VehicleType of the vehicle
+     */
     fun getVehicleType(): VehicleType {
         return vehicleType
     }
 
+    /**
+     * getter for the staffCapacity
+     * @return staffCapacity of the vehicle
+     */
     fun getStaffCapacity(): Int {
         return staffCapacity
     }
 
+    /**
+     * getter for the VehicleHeight
+     * @return Height of the vehicle
+     */
     fun getVehicleHeight(): Int {
         return vehicleHeight
     }
 
+    /**
+     * getter for the baseWaitingTicks
+     * @return baseWaitingTicks of the vehicle
+     */
     fun getBaseWaitingTicks(): Int {
         return baseWaitingTicks
     }
+
+    /**
+     * setter for the baseWaitingTicks
+     */
     fun setBaseWaitingTicks(ticks: Int) {
         baseWaitingTicks = ticks
     }
 
+    /**
+     * getter for the Position
+     * @return Position of the vehicle
+     */
     fun getPosition(): Position? {
         return position
     }
+
+    /**
+     * setter for the Position
+     */
     fun setPosition(pos: Position) {
         position = pos
     }
 
+    /**
+     * getter for the Availability
+     * @return Availability of the vehicle
+     */
     fun isAvailable(): Boolean {
         return available
     }
 
+    /**
+     * setter for the Availability
+     */
     fun setAvailable(value: Boolean) {
         available = value
     }
 
+    /**
+     * moves the vehicle, changes the position
+     */
     fun move() {
         position!!.advance()
     }
 
+    /**
+     * checks if the vehicle is reroutable
+     * @return true if reroutable, else false
+     */
     fun reroutable(): Boolean {
-
         if (position == null) {
             return false
         }
@@ -69,6 +121,10 @@ open class Vehicle(
         return true
     }
 
+    /**
+     * checks if the vehicle is reallocatable
+     * @return true if reallocatable, else false
+     */
     fun reallocatable(em: Emergency): Boolean {
         if (position == null) {
             return false
@@ -80,20 +136,22 @@ open class Vehicle(
             return false
         }
 
-        if(baseWaitingTicks != 0 ){
+        if (baseWaitingTicks != 0) {
             return false
         }
         return true
     }
 
+    /**
+     * reroute the vehicle, change position
+     * @return true if changed the position, else false
+     */
     fun reroute(): Boolean {
-        Position pos = Dijkstra.dijkstraHeigth()
-        if (!position.equals(pos)){
-            position = pos
-            return true
-        }
-        return false
+        // Position pos = Dijkstra.dijkstraHeigth()
+        // if (!position.equals(pos)){
+        // position = pos
+        return true
+        // }
+        // return false }
     }
-
-
 }
