e71150407ed9c12d06304cdfbeca4ba9f87337ba
Lara Hombrecher <laho00003@stud.uni-saarland.de>
2023-09-26 10:59:30 +0200

    ig i fxed the defekt issues? I am not too sure tho

--- src/main/kotlin/de/unisaarland/cs/se/selab/ResourceFactory.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/ResourceFactory.kt
@@ -1,37 +1,69 @@
 package de.unisaarland.cs.se.selab
 
+/**
+ * creates Resources in the parser for Emergencies
+ */
 class ResourceFactory {
 
     fun createFireResources(severity: Int): Resource {
         if (severity == 1) {
             return Resource(mutableListOf(VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER), 1200, 0, 0, null)
         } else if (severity == 2) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER,
-                VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_LADDER, VehicleType.FIREFIGHTER_TRANSPORTER,
-                VehicleType.AMBULANCE)
+            val vehicleList = mutableListOf<VehicleType>(
+                VehicleType.FIRE_TRUCK_WATER,
+                VehicleType.FIRE_TRUCK_WATER,
+                VehicleType.FIRE_TRUCK_WATER,
+                VehicleType.FIRE_TRUCK_WATER,
+                VehicleType.FIRE_TRUCK_LADDER,
+                VehicleType.FIREFIGHTER_TRANSPORTER,
+                VehicleType.AMBULANCE
+            )
             return Resource(vehicleList, 3000, 0, 1, 30)
         } else if (severity == 3) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER,
+            val vehicleList = mutableListOf<VehicleType>(
+                VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER,
                 VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER,
                 VehicleType.FIRE_TRUCK_LADDER, VehicleType.FIRE_TRUCK_LADDER, VehicleType.FIREFIGHTER_TRANSPORTER,
-                VehicleType.FIREFIGHTER_TRANSPORTER, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR)
+                VehicleType.FIREFIGHTER_TRANSPORTER, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR
+            )
             return Resource(vehicleList, 5400, 0, 2, 40)
         }
         return Resource(mutableListOf<VehicleType>(), 0, 0, 0, null)
     }
+    private fun createFireResource3(): Resource {
+        val vehicleList = mutableListOf<VehicleType>(
+            VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER,
+            VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER,
+            VehicleType.FIRE_TRUCK_LADDER, VehicleType.FIRE_TRUCK_LADDER, VehicleType.FIREFIGHTER_TRANSPORTER,
+            VehicleType.FIREFIGHTER_TRANSPORTER, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR
+        )
+        val waterConstant = 5400
+        val patientConstant = 2
+        val ladderConstant = 40
+        return Resource(vehicleList, waterConstant, 0, patientConstant, ladderConstant)
+    }
 
     fun createAccidentResources(severity: Int): Resource {
         if (severity == 1) {
-            return Resource(mutableListOf<VehicleType>(VehicleType.FIRE_TRUCK_TECHNICAL), 0, 0,0, null)
+            return Resource(mutableListOf<VehicleType>(VehicleType.FIRE_TRUCK_TECHNICAL), 0, 0, 0, null)
         } else if (severity == 2) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_CAR,
-                VehicleType.AMBULANCE)
+            val vehicleList =
+                mutableListOf<VehicleType>(
+                    VehicleType.FIRE_TRUCK_TECHNICAL,
+                    VehicleType.FIRE_TRUCK_TECHNICAL,
+                    VehicleType.POLICE_MOTORCYCLE,
+                    VehicleType.POLICE_CAR,
+                    VehicleType.AMBULANCE
+                )
             return Resource(vehicleList, 0, 0, 1, null)
         } else if (severity == 3) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL,
-                VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR,
-                VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR)
-            return  Resource(vehicleList, 0, 0, 2, null)
+            val vehicleList =
+                mutableListOf<VehicleType>(
+                    VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL, VehicleType.FIRE_TRUCK_TECHNICAL,
+                    VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR,
+                    VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR
+                )
+            return Resource(vehicleList, 0, 0, 2, null)
         }
         return Resource(mutableListOf<VehicleType>(), 0, 0, 0, null)
     }
@@ -40,28 +72,47 @@ class ResourceFactory {
         if (severity == 1) {
             return Resource(mutableListOf<VehicleType>(VehicleType.POLICE_CAR), 0, 1, 0, null)
         } else if (severity == 2) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR,
-                VehicleType.K9_POLICE_CAR, VehicleType.AMBULANCE)
+            val vehicleList =
+                mutableListOf<VehicleType>(
+                    VehicleType.POLICE_CAR,
+                    VehicleType.POLICE_CAR,
+                    VehicleType.POLICE_CAR,
+                    VehicleType.POLICE_CAR,
+                    VehicleType.K9_POLICE_CAR,
+                    VehicleType.AMBULANCE
+                )
             return Resource(vehicleList, 0, 4, 0, null)
         } else if (severity == 3) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR,
-                VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.K9_POLICE_CAR, VehicleType.K9_POLICE_CAR,
-                VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_MOTORCYCLE, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.FIREFIGHTER_TRANSPORTER)
+            val vehicleList =
+                mutableListOf<VehicleType>(
+                    VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.POLICE_CAR,
+                    VehicleType.POLICE_CAR, VehicleType.POLICE_CAR, VehicleType.K9_POLICE_CAR, VehicleType.K9_POLICE_CAR,
+                    VehicleType.POLICE_MOTORCYCLE, VehicleType.POLICE_MOTORCYCLE, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.FIREFIGHTER_TRANSPORTER
+                )
             return Resource(vehicleList, 0, 8, 1, null)
         }
         return Resource(mutableListOf<VehicleType>(), 0, 0, 0, null)
     }
-    //patientamount seems unlikely wtf??
+
+    // patientamount seems unlikely wtf??
     fun createMedicalResources(severity: Int): Resource {
         if (severity == 1) {
             return Resource(mutableListOf<VehicleType>(VehicleType.AMBULANCE), 0, 0, 0, null)
         } else if (severity == 2) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR)
+            val vehicleList =
+                mutableListOf<VehicleType>(
+                    VehicleType.AMBULANCE,
+                    VehicleType.AMBULANCE,
+                    VehicleType.EMERGENCY_DOCTOR_CAR
+                )
             return Resource(vehicleList, 0, 0, 2, null)
         } else if (severity == 3) {
-            val vehicleList = mutableListOf<VehicleType>(VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.AMBULANCE,
-                VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR, VehicleType.EMERGENCY_DOCTOR_CAR, VehicleType.FIRE_TRUCK_TECHNICAL,
-                VehicleType.FIRE_TRUCK_TECHNICAL)
+            val vehicleList =
+                mutableListOf<VehicleType>(
+                    VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.AMBULANCE, VehicleType.AMBULANCE,
+                    VehicleType.AMBULANCE, VehicleType.EMERGENCY_DOCTOR_CAR, VehicleType.EMERGENCY_DOCTOR_CAR, VehicleType.FIRE_TRUCK_TECHNICAL,
+                    VehicleType.FIRE_TRUCK_TECHNICAL
+                )
             return Resource(vehicleList, 0, 0, 5, null)
         }
         return Resource(mutableListOf<VehicleType>(), 0, 0, 0, null)
