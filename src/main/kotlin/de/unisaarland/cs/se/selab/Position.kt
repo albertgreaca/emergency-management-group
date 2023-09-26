86f73e63999332036402ffff2a9c270af1f05a39
Albert-Antoniu Greaca <algr00003@stud.uni-saarland.de>
2023-09-26 10:46:53 +0200

    heelp

--- src/main/kotlin/de/unisaarland/cs/se/selab/Position.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Position.kt
@@ -1,32 +1,84 @@
 package de.unisaarland.cs.se.selab
 
-import java.lang.Integer.min
-
+/** Class for the position of a vehicle
+ */
 class Position(
-    var roadList: MutableList<Road>,
-    var roadIndex: Int,
-    var positionOnRoad: Int,
-    var destinationVertex: Vertex?,
-    var arrivalTicks: Int,
-    var distance: Int,
-    var startedThisTick: Boolean
+    private var roadList: MutableList<Road>,
+    private var roadIndex: Int,
+    private var positionOnRoad: Int,
+    private var destinyVertex: Vertex,
+    private var arrivalTicks: Int,
+    private var startedThisTick: Boolean
 ) {
+    /**
+     * @return list of roads the vehicle has to drive on
+     * the path, way
+     */
+    fun getRoadList(): MutableList<Road> {
+        return roadList
+    }
 
+    /**
+     * @return index in the road list
+     * road the vehicle is driving on
+     */
+    fun getRoadIndex(): Int {
+        return roadIndex
+    }
+
+    /**
+     * @return actual weight on the road where the vehicle is
+     */
+    fun getPositionOnRoad(): Int {
+        return positionOnRoad
+    }
+
+    /**
+     * @return vertex of the destination
+     */
+    fun getDestinyVertex(): Vertex {
+        return destinyVertex
+    }
+
+    /**
+     * @return needed ticks to arrive
+     */
+    fun getArrivalTicks(): Int {
+        return arrivalTicks
+    }
+
+    /**
+     * @return true if vehicle just started driving in this tick, else false
+     */
+    fun getStartedThisTick(): Boolean {
+        return startedThisTick
+    }
+
+    /**
+     * setter for the arrival ticks
+     */
+    fun setArrivalTicks(ticks: Int) {
+        arrivalTicks = ticks
+    }
+
+    /**
+     * changing the position after vehicle was updated
+     */
     fun advance() {
         //TODO
     }
 
-    fun isEqual(o: Position): Boolean {
-        return this.roadList == o.roadList && this.roadIndex == o.roadIndex &&
-            this.positionOnRoad == o.positionOnRoad &&
-            this.destinationVertex == o.destinationVertex &&
-            this.arrivalTicks == o.arrivalTicks
-    }
-    fun smaller(o: Position): Boolean {
-        // TODO
-        /*var n: Int = roadList.size
-        var m: Int = o.roadList.size
-        for (i in 0..min(n,m) - 1)
-            */
+    /**
+     * equals method for positions
+     */
+    override fun equals(o: Any?): Boolean {
+        if (o is Position) {
+            return this.getRoadList() == o.getRoadList() &&
+                    this.roadIndex == o.getRoadIndex() &&
+                    this.getPositionOnRoad() == o.getPositionOnRoad() &&
+                    this.getDestinyVertex() == o.getDestinyVertex() &&
+                    this.getArrivalTicks() == o.getArrivalTicks()
+        }
+        return false
     }
 }
