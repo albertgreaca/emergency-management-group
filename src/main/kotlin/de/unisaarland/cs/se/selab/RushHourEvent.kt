847816062d5becf946c4651ce021b88c0931b5e1
Sude Ergün <suer00002@stud.uni-saarland.de>
2023-09-26 10:26:14 +0200

    events start

--- src/main/kotlin/de/unisaarland/cs/se/selab/RushHourEvent.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/RushHourEvent.kt
@@ -1,4 +1,20 @@
 package de.unisaarland.cs.se.selab
-class RushHourEvent {
+class RushHourEvent(
+    private val id: Int,
+    private var tick: Int,
+    private var duration: Int,
+    private var roads: MutableList<Road>,
+    private val factor: Int
+) : Event(
+    id,
+    tick,
+    duration
+) {
+    fun executeStart(): Boolean{
 
+    }
+
+    fun updateEvent(): Boolean{
+        if(tick+duration == )
+    }
 }
