061f5751a5f2b87f49a57aad2f2f562210f51872
Davide <DavideDamiano1134@gmail.com>
2023-09-27 11:47:56 +0200

    GraphMapTest vertex constructor adapted

--- src/test/kotlin/GraphMapTest.kt
+++ src/test/kotlin/GraphMapTest.kt
@@ -11,19 +11,19 @@ class GraphMapTest {
         val parser = MapParser(gm, File("src/test/resources/mapvalid1.dot"))
         parser.parseMap()
         val roadList = gm.roadList
-        val vert0 = Vertex(0, null)
-        val vert1 = Vertex(1, null)
-        val vert2 = Vertex(2, null)
+        val vert0 = Vertex(0, null, 0)
+        val vert1 = Vertex(1, null, 1)
+        val vert2 = Vertex(2, null, 2)
         val r1 =
             Road(PrimaryRoadType.MAINSTREET, SecondaryRoadType.NONE, "Saarbruecken", "Teststrasse", 10, 5, vert1, vert2)
         assertTrue(roadList.contains(r1))
         val r2 = Road(PrimaryRoadType.SIDESTREET, SecondaryRoadType.NONE, "Saarbrücken", "Campus", 10, 1, vert0, vert1)
         assertTrue(roadList.contains(r2))
-        val adjList: MutableList<MutableMap<Vertex,Road>> = gm.adjacencyList
+        val adjList: MutableList<MutableMap<Vertex, Road>> = gm.adjacencyList
         val map0 = adjList[0]
         assertTrue(map0.containsKey(vert1)) //check existence of connection between vert0 and vert1
         val supposed2 = map0[vert1]
-            assertTrue(supposed2 == r2) //check if the connection between vert0 and vert1 is supposed one
+        assertTrue(supposed2 == r2) //check if the connection between vert0 and vert1 is supposed one
         val map1 = adjList[1]
         assertTrue(map1.containsKey(vert0)) // check if the said connection is bidirectional
         val supposed2Again = map1[vert0]
