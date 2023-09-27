9a6ff1bc634afc77172dc326968877b2e4c47882
Benedict Bliem <benedict.bliem@cgm.com>
2023-09-27 19:18:36 +0200

    changed Logger methods to return printed string

--- src/main/kotlin/de/unisaarland/cs/se/selab/Logger.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Logger.kt
@@ -20,127 +20,161 @@ object Logger {
     /**
      *
      */
-    fun logInitInfo(filename: String, isValid: Boolean) {
+    fun logInitInfo(filename: String, isValid: Boolean): String {
+        var output = ""
         if (isValid) {
-            pw.print("Initialization Info: $filename successfully parsed and validated")
+            output = "Initialization Info: $filename successfully parsed and validated"
+            pw.print(output)
         } else {
-            pw.print("Initialization Info: $filename invalid")
+            output = "Initialization Info: $filename invalid"
+            pw.print(output)
         }
+        return output
     }
 
     /**
      *
      */
-    fun logSimulationStart() {
-        pw.print("Simulation starts")
+    fun logSimulationStart(): String {
+        val output = "Simulation starts"
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logSimulationTick(t: Int) {
-        pw.print("Simulation Tick: $t")
+    fun logSimulationTick(t: Int): String {
+        val output = "Simulation Tick: $t"
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logEmergencyAssignment(eid: Int, bid: Int) {
-        pw.print("Emergency Assignment: $eid assigned to $bid")
+    fun logEmergencyAssignment(eid: Int, bid: Int): String {
+        val output = "Emergency Assignment: $eid assigned to $bid"
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logAssetAllocation(aid: Int, eid: Int, t: Int) {
-        pw.print("Asset Allocation: $aid allocated to $eid; $t ticks to arrive.")
+    fun logAssetAllocation(aid: Int, eid: Int, t: Int): String {
+        val output = "Asset Allocation: $aid allocated to $eid; $t ticks to arrive."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logAssetReallocation(aid: Int, eid: Int) {
-        pw.print("Asset Reallocation: $aid reallocated to $eid.")
+    fun logAssetReallocation(aid: Int, eid: Int): String {
+        val output = "Asset Reallocation: $aid reallocated to $eid."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logAssetRequest(rid: Int, bid: Int, eid: Int) {
-        pw.print("Asset Request: $rid sent to $bid for $eid.")
+    fun logAssetRequest(rid: Int, bid: Int, eid: Int): String {
+        val output = "Asset Request: $rid sent to $bid for $eid."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logRequestFailed(eid: Int) {
-        pw.print("Request Failed: $eid failed.")
+    fun logRequestFailed(eid: Int): String {
+        val output = "Request Failed: $eid failed."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logAssetArrival(aid: Int, vid: Int) {
-        pw.print("Asset Arrival: $aid arrived at $vid.")
+    fun logAssetArrival(aid: Int, vid: Int): String {
+        val output = "Asset Arrival: $aid arrived at $vid."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logEmergencyHandlingStart(eid: Int) {
-        pw.print("Emergency Handling Start: $eid handling started.")
+    fun logEmergencyHandlingStart(eid: Int): String {
+        val output = "Emergency Handling Start: $eid handling started."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logEmergencyResolved(eid: Int) {
-        pw.print("Emergency Resolved: $eid resolved.")
+    fun logEmergencyResolved(eid: Int): String {
+        val output = "Emergency Resolved: $eid resolved."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logEmergencyFailed(eid: Int) {
-        pw.print("Emergency Failed: $eid failed.")
+    fun logEmergencyFailed(eid: Int): String {
+        val output = "Emergency Failed: $eid failed."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logEventEnded(evid: Int) {
-        pw.print("Event Ended: $evid ended.")
+    fun logEventEnded(evid: Int): String {
+        val output = "Event Ended: $evid ended."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logEventTriggered(evid: Int) {
-        pw.print("Event Triggered: $evid triggered.")
+    fun logEventTriggered(evid: Int): String {
+        val output = "Event Triggered: $evid triggered."
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logAssetsRerouted(numOfAssets: Int) {
-        pw.print("Assets Rerouted: $numOfAssets")
+    fun logAssetsRerouted(numOfAssets: Int): String {
+        val output = "Assets Rerouted: $numOfAssets"
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logSimulationEnd() {
-        pw.print("Simulation End")
+    fun logSimulationEnd(): String {
+        val output = "Simulation End"
+        pw.print(output)
+        return output
     }
 
     /**
      *
      */
-    fun logStatistics(rerouted: Int, received: Int, ongoing: Int, failed: Int, resolved: Int) {
-        pw.print("Simulation Statistics: $rerouted assets rerouted.")
-        pw.print("Simulation Statistics: $received received emergencies.")
-        pw.print("Simulation Statistics: $ongoing ongoing emergencies.")
-        pw.print("Simulation Statistics: $failed failed emergencies.")
-        pw.print("Simulation Statistics: $resolved resolved emergencies.")
+    fun logStatistics(rerouted: Int, received: Int, ongoing: Int, failed: Int, resolved: Int): String {
+        val output = "Simulation Statistics: $rerouted assets rerouted.\nSimulation Statistics: $received received " +
+                "emergencies.\nSimulation Statistics: $ongoing ongoing emergencies.\nSimulation Statistics: $failed " +
+                "failed emergencies.\nSimulation Statistics: $resolved resolved emergencies."
+        pw.print(output)
+        return output
     }
 }
