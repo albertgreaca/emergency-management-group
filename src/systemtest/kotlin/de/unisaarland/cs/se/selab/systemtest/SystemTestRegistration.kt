8b1012677c9eee2bf5d8532bfeba3d5db3372d06
Niklas Scholz <nisc00019@stud.uni-saarland.de>
2023-09-28 07:06:39 +0200

    Forgot one test

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -1,10 +1,10 @@
 package de.unisaarland.cs.se.selab.systemtest
-import de.unisaarland.cs.se.selab.systemtest.basictests.ExampleTest
 import de.unisaarland.cs.se.selab.systemtest.failedmapparser.*
 import de.unisaarland.cs.se.selab.systemtest.runner.SystemTestManager
 
 object SystemTestRegistration {
     fun registerSystemTests(manager: SystemTestManager) {
+        manager.toString()
         /*manager.registerTest(ExampleTest())
         manager.registerTest(EmergencySimpleTest())
         manager.registerTest(ReroutingTest())
