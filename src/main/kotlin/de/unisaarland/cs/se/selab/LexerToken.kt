b346cdbacf96556c1b62679841102c12106275bc
Lara Hombrecher <laho00003@stud.uni-saarland.de>
2023-09-28 13:11:47 +0200

    added Token enums for lexing

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/LexerToken.kt
@@ -0,0 +1,23 @@
+package de.unisaarland.cs.se.selab
+
+enum class LexerToken {
+    ARROW,
+    SEMICOLON,
+    RPARENTHESES,
+    LPARENTHESES,
+    CLPARENTHESES,
+    CRPARENTHESES,
+    EQUAL,
+    VILLAGE,
+    NAME,
+    HEIGHTLIMIT,
+    WEIGHT,
+    PRIMARYTYPE,
+    SECONDARYTYPE,
+    MAINSTREET,
+    SIDESTREET,
+    COUNTYROAD,
+    ONEWAYSTREET,
+    TUNNEL,
+    NONE
+}
