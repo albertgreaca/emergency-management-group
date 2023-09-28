a677f496cbd43e49bd24f5ee717521c2ae4207de
Lara Hombrecher <laho00003@stud.uni-saarland.de>
2023-09-28 20:08:46 +0200

    I do have a working regex now

--- src/main/kotlin/de/unisaarland/cs/se/selab/Lexer.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Lexer.kt
@@ -4,9 +4,45 @@ class Lexer {
 
 
     // function instream needed for character List?
-    fun lex(s: String) {
-        s.replace("\\s+".toRegex(), " ").trim()
-        s.split("\\s+".toRegex()).toMutableList()
+    fun lex(s: String): MutableList<LexerToken> {
+
+        var string = s.replace("\\s+}".toRegex(), " ").trim()
+        string = string.replace("\n"," ")
+        string = string.replace(";", " ; ")
+        string = string.replace("[", " [ ")
+        string = string.replace("]", " ] ")
+        string = string.replace("{", " { ")
+        string = string.replace("}", " } ")
+        string = string.replace("=", " = ")
+        string = string.replace("-", " -")
+        string = string.replace(">", "> ")
+        var list = string.split("\\s+".toRegex()).toMutableList()
+        var tokenlist = mutableListOf<LexerToken>()
+        for (element in list) {
+            when(element) {
+                "->" -> tokenlist.add(LexerToken.ARROW)
+                ";" -> tokenlist.add(LexerToken.SEMICOLON)
+                ")" -> tokenlist.add(LexerToken.RPARENTHESES)
+                "(" -> tokenlist.add(LexerToken.LPARENTHESES)
+                "{" -> tokenlist.add(LexerToken.CLPARENTHESES)
+                "}" -> tokenlist.add(LexerToken.CRPARENTHESES)
+                "=" -> tokenlist.add(LexerToken.EQUAL)
+                "village" -> tokenlist.add(LexerToken.VILLAGE)
+                "name" -> tokenlist.add(LexerToken.NAME)
+                "heightLimit" -> tokenlist.add(LexerToken.HEIGHTLIMIT)
+                "weight" -> tokenlist.add(LexerToken.WEIGHT)
+                "primaryType" -> tokenlist.add(LexerToken.PRIMARYTYPE)
+                "secondaryType" -> tokenlist.add(LexerToken.SECONDARYTYPE)
+                "mainStreet" -> tokenlist.add(LexerToken.MAINSTREET)
+                "sideStreet" -> tokenlist.add(LexerToken.SIDESTREET)
+                "countyRoad" -> tokenlist.add(LexerToken.COUNTYROAD)
+                "oneWayStreet" -> tokenlist.add(LexerToken.ONEWAYSTREET)
+                "tunnel" -> tokenlist.add(LexerToken.TUNNEL)
+                "none" -> tokenlist.add(LexerToken.NONE)
+            }
+        }
+
+        return tokenlist
     }
 
     fun lexString() {
---
+++ src/test/kotlin/LexerTest.kt
@@ -0,0 +1,17 @@
+import de.unisaarland.cs.se.selab.Lexer
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class LexerTest {
+
+    @Test
+    fun simpletest() {
+        val lex = Lexer()
+        val string = lex.lex("Test {\n" +
+                "0;\n" +
+                "1;\n" +
+                "2;\n" +
+                "0->1[village =")
+        assertTrue(false, string.toString())
+    }
+}
