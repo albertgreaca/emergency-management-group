a92ebf0be632f3eb682de474a699af033cd04e55
Niklas Scholz <nisc00019@stud.uni-saarland.de>
2023-09-29 09:14:47 +0200

    Restored Example Test to make sure it might not be our error why test is failing

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/failedmapparser/FailedParser3.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/failedmapparser/FailedParser3.kt
@@ -3,7 +3,7 @@ package de.unisaarland.cs.se.selab.systemtest.failedmapparser
 import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
 
 class FailedParser3 : SystemTest() {
-    override val name = "FailedParser1"
+    override val name = "FailedParser3"
 
     override val map = "invalidMaps/SemanticIssues/mapinvalid3.dot"
     override val assets = "assetsJsons/example_assets.json"
