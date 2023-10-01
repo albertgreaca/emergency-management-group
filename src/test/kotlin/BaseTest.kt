0f368b3e042a9fae741a1314acad5dda15f4e915
Lara Hombrecher <laho00003@stud.uni-saarland.de>
2023-10-01 15:03:32 +0200

    one more base test but unsure about specification

--- src/test/kotlin/BaseTest.kt
+++ src/test/kotlin/BaseTest.kt
@@ -71,12 +71,12 @@ class BaseTest {
 
     @Test
     fun watertrucktest1200w2vehic() {
-        // some more cases i thought of:
+        // some more cases I thought of:
         // 2400, 1 vehic
-        // 2400, 2 vehic
         // 1800, 3 vehic
         // 1800, 1 vehic
         // 600, 1 vehic
+        // understaffed so only 1 vehicle can be sent
         val graph = Simulation.map
         val parse = MapParser(graph, File("src/test/resources/mapvalid1.dot"))
         val jsonparse = JsonParser(
@@ -107,5 +107,45 @@ class BaseTest {
         assertTrue(em.resources.isEmpty())
         assertTrue(vehicle1 == em.assignedVehicles[0])
         assertTrue(vehicle2 == em.assignedVehicles[1])
+    }
+
+    @Test
+    fun watertrucktest2400w2vehic() {
+        // some more cases I thought of:
+        // 2400, 1 vehic
+        // 1800, 3 vehic
+        // 1800, 1 vehic
+        // 600, 1 vehic
+        // understaffed so only 1 vehicle can be sent
+        val graph = Simulation.map
+        val parse = MapParser(graph, File("src/test/resources/mapvalid1.dot"))
+        val jsonparse = JsonParser(
+            graph,
+            File("src/test/resources/UnitTestConfig2/firebasesimple.json"),
+            File("src/test/resources/UnitTestConfig2/emergencysimple.json")
+        )
+        parse.parseMap()
+        jsonparse.parseBases()
+        jsonparse.parseVehicles()
+        jsonparse.parseEmergency()
+        jsonparse.parseEvents()
+        val vertex1 = requireNotNull(graph.getVertexFromId(0))
+        val vertex2 = requireNotNull(graph.getVertexFromId(1))
+        val road = requireNotNull(graph.getRoad(vertex1, vertex2))
+        val vehicleList = mutableListOf(VehicleType.FIRE_TRUCK_WATER, VehicleType.FIRE_TRUCK_WATER)
+        val res = Resource(vehicleList, 2400, 0, 0, 0)
+        val em = Emergency(0, 1, road, EmergencyType.FIRE, 2, 1, 20, res)
+        em.id
+        val vertex3 = requireNotNull(graph.getVertexFromId(2))
+        val b = requireNotNull(vertex3.base)
+        em.base = b
+        b.requestResources(em)
+        // in this case, should the base send a 600 Watertruck, as the 1800 can stil be fulfiled with a 2400?
+        // val vehicles = b.vehicles
+        // val vehicle1 = vehicles[3]
+        // val testres = Resource(mutableListOf(VehicleType.FIRE_TRUCK_WATER), 1200, 0, 0, 0)
+        // assertTrue(em.resources.isEqual(testres))
+        // assertTrue(vehicle1 == em.assignedVehicles[0])
+        assertTrue(true)
     }
 }
