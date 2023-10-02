bacb06b49bd216c75c64c418b4ad5e79f76ea8db
Lara Hombrecher <laho00003@stud.uni-saarland.de>
2023-10-02 12:40:28 +0200

    json testing could be problematic, ill look into it later

---
+++ src/test/kotlin/JsonParserConfig3Semantics.kt
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class JsonParserConfig3Semantics {
+    @Test
+    fun invalidTestSemantics1() {
+        assertTrue(true)
+    }
+}
