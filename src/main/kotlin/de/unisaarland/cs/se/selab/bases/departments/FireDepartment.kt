7f2540d93f2f023c1d16eb0d6a15e0e7933fd6d8
Benedict Bliem <benedict.bliem@cgm.com>
2023-10-02 17:07:17 +0200

    fixed bug in department classes and added new tests testing for this error

--- src/main/kotlin/de/unisaarland/cs/se/selab/bases/departments/FireDepartment.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/bases/departments/FireDepartment.kt
@@ -47,9 +47,9 @@ class FireDepartment : EmergencyObserver {
             for (v in b.vehicles) {
                 if (v.baseWaitingTicks > 1) {
                     v.baseWaitingTicks--
-                }
-                if (v.baseWaitingTicks == 1) {
+                } else if (v.baseWaitingTicks == 1) {
                     v.available = true
+                    v.baseWaitingTicks--
                 }
             }
         }
