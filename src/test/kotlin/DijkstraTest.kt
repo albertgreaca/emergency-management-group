24de6e40bd2454767fdd0840132296696a693223
Ruwen Gebhardt <ruge00001@stud.uni-saarland.de>
2023-10-03 20:25:17 +0200

    BackToBase Test

--- src/test/kotlin/DijkstraTest.kt
+++ src/test/kotlin/DijkstraTest.kt
@@ -135,4 +135,24 @@ class DijkstraTest {
 
         assertTrue(posDijkstra.isEqual(posComparable))
     }
+
+    @Test
+    fun dijkstraBackToBaseTest() {
+        val parser = MapParser(Simulation.map, File("src/systemtest/resources/mapFiles/MapEmergencySimple.dot"))
+        parser.parseMap()
+        val road = requireNotNull(Simulation.map.getRoad("Campus", "Chemistry"))
+        val posdijkstra: Position = requireNotNull(Dijkstra.dijkstraBackToBase(0, 3, 5))
+        val road1 = requireNotNull(Simulation.map.getRoad("Campus", "Medieninformatik"))
+        val road2 = requireNotNull(Simulation.map.getRoad("UdS", "Naturkunst"))
+        val road3 = requireNotNull(Simulation.map.getRoad("UdS", "Biochemie"))
+        val vertex0 = Simulation.map.getVertexFromId(1) ?: return
+        val vertex1 = Simulation.map.getVertexFromId(3) ?: return
+        val vertex2 = Simulation.map.getVertexFromId(2) ?: return
+        val vertex3 = Simulation.map.getVertexFromId(4) ?: return
+        val vertexList = mutableListOf(vertex0, vertex1, vertex2, vertex3)
+        val roadList = mutableListOf(road1, road2, road3)
+        val posway: Position = Position(roadList, vertexList, 0, 7, vertex1, 25, 3, false, false)
+
+        assertTrue(posdijkstra.isEqual(posway))
+    }
 }
