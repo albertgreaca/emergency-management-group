c02288ceec9d5f0b42d4e9d54999af3a6792f111
Ruwen Gebhardt <ruge00001@stud.uni-saarland.de>
2023-10-03 11:51:47 +0200

    dijkstraHight test

--- src/test/kotlin/DijkstraTest.kt
+++ src/test/kotlin/DijkstraTest.kt
@@ -1,5 +1,10 @@
+import de.unisaarland.cs.se.selab.graphlogic.Dijkstra
+import de.unisaarland.cs.se.selab.mainlogic.Simulation
+import de.unisaarland.cs.se.selab.parser.MapParser
+import de.unisaarland.cs.se.selab.utils.Position
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertTrue
 
 class DijkstraTest {
@@ -12,13 +17,21 @@ class DijkstraTest {
 
     @Test
     fun dijkstraHeight() {
-        /*
         val parser = MapParser(Simulation.map, File("src/systemtest/resources/mapFiles/MapEmergencySimple.dot"))
-        val road = Simulation.map.getRoad("Campus", "Chemistry") ?: return
-        val posdijkstra: Position? = Dijkstra.dijkstraHeight(1, road, 5)
-        val roadList = mutableListOf<Road>()
-        // val posway: Position = Position(mutableListOf(), )
-    */
-        assertTrue(true)
+        parser.parseMap()
+        val road = requireNotNull(Simulation.map.getRoad("Campus", "Chemistry"))
+        val posdijkstra: Position = requireNotNull(Dijkstra.dijkstraHeight(0, road, 5))
+        val road1 = requireNotNull(Simulation.map.getRoad("Campus", "Medieninformatik"))
+        val road2 = requireNotNull(Simulation.map.getRoad("UdS", "Naturkunst"))
+        val road3 = requireNotNull(Simulation.map.getRoad("UdS", "Biochemie"))
+        val vertex0 = Simulation.map.getVertexFromId(1) ?: return
+        val vertex1 = Simulation.map.getVertexFromId(3) ?: return
+        val vertex2 = Simulation.map.getVertexFromId(2) ?: return
+        val vertex3 = Simulation.map.getVertexFromId(4) ?: return
+        val vertexList = mutableListOf(vertex0, vertex1, vertex2, vertex3)
+        val roadList = mutableListOf(road1, road2, road3)
+        val posway: Position = Position(roadList, vertexList, 0, 7, vertex1, 25, 3, false, false)
+
+        assertTrue(posdijkstra.isEqual(posway))
     }
 }
