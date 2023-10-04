98f0e25a56da53ac1ebd8557a66c5ac995775721
Davide <DavideDamiano1134@gmail.com>
2023-10-04 23:00:26 +0200

    Some fixes and some sys test

--- src/main/kotlin/de/unisaarland/cs/se/selab/vehicles/Ambulance.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/vehicles/Ambulance.kt
@@ -1,6 +1,8 @@
 package de.unisaarland.cs.se.selab.vehicles
 
 import de.unisaarland.cs.se.selab.bases.Base
+import de.unisaarland.cs.se.selab.emergencies.Emergency
+import de.unisaarland.cs.se.selab.mainlogic.EMCC
 import de.unisaarland.cs.se.selab.utils.Position
 
 /** Class for the Ambulance
@@ -19,4 +21,22 @@ data class Ambulance(
     staffCapacity,
     vehicleHeight,
     position
-)
+) {
+    override fun reallocatable(em: Emergency): Boolean {
+        if (position == null) {
+            return false
+        }
+        if (requireNotNull(position).arrivalTicks == 0) {
+            return false
+        }
+        if (baseWaitingTicks != 0 || patientOnBoard) {
+            return false
+        }
+        if (targetEmergency != null && !EMCC.resolvedOrFailedEmergencies.contains(targetEmergency) &&
+            em.severity <= requireNotNull(targetEmergency).severity
+        ) {
+            return false
+        }
+        return true
+    }
+}
--- src/main/kotlin/de/unisaarland/cs/se/selab/vehicles/FireTruckWater.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/vehicles/FireTruckWater.kt
@@ -1,6 +1,8 @@
 package de.unisaarland.cs.se.selab.vehicles
 
 import de.unisaarland.cs.se.selab.bases.Base
+import de.unisaarland.cs.se.selab.emergencies.Emergency
+import de.unisaarland.cs.se.selab.mainlogic.EMCC
 import de.unisaarland.cs.se.selab.utils.Position
 
 /** Class for the Fire Truck Water
@@ -21,4 +23,20 @@ class FireTruckWater(
     position
 ) {
     var waterTransported: Int = waterCapacity
+
+    override fun reallocatable(em: Emergency): Boolean {
+        if (position == null) {
+            return false
+        }
+        if (requireNotNull(position).arrivalTicks == 0) {
+            return false
+        }
+        if (baseWaitingTicks != 0 || waterTransported == 0) {
+            return false
+        }
+        if (targetEmergency != null && !EMCC.resolvedOrFailedEmergencies.contains(targetEmergency) && em.severity <= requireNotNull(targetEmergency).severity) {
+            return false
+        }
+        return true
+    }
 }
