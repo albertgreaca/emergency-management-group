da100fe616851175295e884a8250c58b36279134
Benedict Bliem <benedict.bliem@cgm.com>
2023-10-04 17:17:24 +0200

    added new system tests for invalid config3 files with semantic errors

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/failedconfig3parser/semantics/FailedParserConfig3Semantics9.kt
@@ -0,0 +1,16 @@
+package de.unisaarland.cs.se.selab.systemtest.failedconfig3parser.semantics
+import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
+
+class FailedParserConfig3Semantics9 : SystemTest() {
+    override val name = "FailedParserConfig3Semantics9"
+
+    override val map = "mapFiles/example_map.dot"
+    override val assets = "src/systemtest/resources/assetsJsons/example_assets.json"
+    override val scenario = "src/systemtest/resources/invalidConfig3/Sematics/config3invalid9.json"
+    override val maxTicks = 1
+    override suspend fun run() {
+        assertNextLine("Initialization Info: example_map.dot successfully parsed and validated")
+        assertNextLine("Initialization Info: example_assets.json successfully parsed and validated")
+        assertNextLine("Initialization Info: config3invalid9.json invalid")
+    }
+}
