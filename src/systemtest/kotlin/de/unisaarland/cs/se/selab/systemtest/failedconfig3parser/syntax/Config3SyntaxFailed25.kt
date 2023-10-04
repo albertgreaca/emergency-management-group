1366ad3da1c4d4d29603d2df4de8842d8dbe74a2
Niklas Scholz <nisc00019@stud.uni-saarland.de>
2023-10-04 18:00:43 +0200

    Fixed

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/failedconfig3parser/syntax/Config3SyntaxFailed25.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/failedconfig3parser/syntax/Config3SyntaxFailed25.kt
@@ -3,11 +3,11 @@ package de.unisaarland.cs.se.selab.systemtest.failedconfig3parser.syntax
 import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
 
 class Config3SyntaxFailed25 : SystemTest() {
-    override val name = "Config3SyntaxFailed24"
+    override val name = "Config3SyntaxFailed25"
 
     override val map = "mapFiles/example_map.dot"
     override val assets = "assetsJsons/example_assets.json"
-    override val scenario = "invalidConfig3/Syntax/config3invalid24.json"
+    override val scenario = "invalidConfig3/Syntax/config3invalid25.json"
     override val maxTicks = 1
     override suspend fun run() {
         assertNextLine("Initialization Info: example_map.dot successfully parsed and validated")
