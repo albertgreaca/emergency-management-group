fde335c93e4354b14e753dbe734434e8d001627b
Davide <DavideDamiano1134@gmail.com>
2023-10-05 20:32:14 +0200

    double realloc agaaain

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/complicated/DoubleRealloc.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/complicated/DoubleRealloc.kt
@@ -25,7 +25,7 @@ class DoubleRealloc : SystemTest() {
         assertNextLine("Asset Allocation: 5 allocated to 1; 3 ticks to arrive.")
         assertNextLine("Asset Reallocation: 3 reallocated to 1.")
         assertNextLine("Asset Request: 1 sent to 3 for 0.") // request for sev 0 em (ambulancia was reallocated)
-        assertNextLine("Asset Allocation: 10 allocated to 0; 4 ticks to arrive.") // ambulance for sev 0
+        assertNextLine("Asset Allocation: 7 allocated to 0; 4 ticks to arrive.") // ambulance for sev 0
         assertNextLine("Simulation Tick: 3")
         assertNextLine("Emergency Assignment: 2 assigned to 2") // process severity 3 emergency
         assertNextLine("Asset Allocation: 6 allocated to 2; 3 ticks to arrive.")
@@ -37,9 +37,9 @@ class DoubleRealloc : SystemTest() {
         assertNextLine("Asset Request: 4 sent to 3 for 1.")
         assertNextLine("Asset Allocation: 1 allocated to 2; 4 ticks to arrive.")
         assertNextLine("Asset Allocation: 2 allocated to 2; 4 ticks to arrive.")
-        assertNextLine("Asset Allocation: 7 allocated to 2; 4 ticks to arrive.") // now the 3 ambulancias for sev 2
         assertNextLine("Asset Allocation: 8 allocated to 2; 4 ticks to arrive.")
         assertNextLine("Asset Allocation: 9 allocated to 2; 4 ticks to arrive.")
+        assertNextLine("Asset Allocation: 10 allocated to 2; 4 ticks to arrive.") // now the 3 ambulancias for sev 2
         assertNextLine("Asset Allocation: 11 allocated to 1; 4 ticks to arrive.")
         assertNextLine("Asset Allocation: 12 allocated to 1; 4 ticks to arrive.")
         assertNextLine("Asset Allocation: 13 allocated to 1; 4 ticks to arrive.")
@@ -50,15 +50,15 @@ class DoubleRealloc : SystemTest() {
         assertNextLine("Simulation Tick: 6")
         assertNextLine("Asset Arrival: 3 arrived 7.")
         assertNextLine("Asset Arrival: 6 arrived 7.")
-        assertNextLine("Asset Arrival: 10 arrived 3.")
+        assertNextLine("Asset Arrival: 7 arrived 3.")
         assertNextLine("Emergency Handling Start: 0 handling started.")
         assertNextLine("Simulation Tick: 7")
         assertNextLine("Asset Arrival: 1 arrived 7.")
         assertNextLine("Asset Arrival: 2 arrived 7.")
         assertNextLine("Asset Arrival: 3 arrived 7.")
-        assertNextLine("Asset Arrival: 7 arrived 7.")
         assertNextLine("Asset Arrival: 8 arrived 7.")
         assertNextLine("Asset Arrival: 9 arrived 7.")
+        assertNextLine("Asset Arrival: 10 arrived 7.")
         assertNextLine("Asset Arrival: 11 arrived 5.")
         assertNextLine("Asset Arrival: 12 arrived 5.")
         assertNextLine("Asset Arrival: 13 arrived 5.")
