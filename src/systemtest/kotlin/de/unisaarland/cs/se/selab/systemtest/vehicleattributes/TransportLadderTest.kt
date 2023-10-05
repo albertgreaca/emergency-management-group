29098bae2def76a3992a7f8cbe70f4d1f78a483b
Niklas Scholz <nisc00019@stud.uni-saarland.de>
2023-10-05 17:05:49 +0200

    Ladder Test

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/vehicleattributes/TransportLadderTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/vehicleattributes/TransportLadderTest.kt
@@ -6,11 +6,11 @@ class TransportLadderTest : SystemTest() {
     override val name = "TransportLadderTest"
 
     override val map = "mapFiles/example_map.dot"
-    override val assets = "jsonvehiclesproperties/TransportLadder.json"
+    override val assets = "jsonvehiclesproperties/TransporterLadder.json"
     override val scenario = "scenarioJsons/example_scenario.json"
     override val maxTicks = 1
     override suspend fun run() {
         assertNextLine("Initialization Info: example_map.dot successfully parsed and validated")
-        assertNextLine("Initialization Info: TransportLadder.json invalid")
+        assertNextLine("Initialization Info: TransporterLadder.json invalid")
     }
 }
