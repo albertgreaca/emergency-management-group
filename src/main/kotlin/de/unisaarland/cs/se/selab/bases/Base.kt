34a1a541711029438764304f2aa3f6ac14fdb62a
Benedict Bliem <benedict.bliem@cgm.com>
2023-10-05 11:05:19 +0200

    fixed bug in allocation phase and added new system test for reallocation

--- src/main/kotlin/de/unisaarland/cs/se/selab/bases/Base.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/bases/Base.kt
@@ -92,6 +92,13 @@ open class Base(
         requireNotNull(vehicle.position).startedThisTick = true
         // reduce the available staff of the vehicle
         this.staff -= vehicle.staffCapacity
+        // reduce special staff
+        if (this is Hospital && vehicle.vehicleType == VehicleType.EMERGENCY_DOCTOR_CAR) {
+            this.doctors--
+        }
+        if (this is PoliceStation && vehicle.vehicleType == VehicleType.K9_POLICE_CAR) {
+            this.dogs--
+        }
         // set the target emergency of the vehicle
         vehicle.targetEmergency = em
         // remove the vehicle type from the list of needed vehicle types
