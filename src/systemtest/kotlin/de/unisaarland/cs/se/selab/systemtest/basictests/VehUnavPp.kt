ed947a43654f059d735c68e6be017517d1dddad1
Davide <DavideDamiano1134@gmail.com>
2023-10-05 22:23:48 +0200

    Te juro que te pienso

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/basictests/VehUnavPp.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/basictests/VehUnavPp.kt
@@ -6,27 +6,44 @@ class VehUnavPp : SystemTest() {
     override val name = "VehUnavPp"
 
     override val map = "mapFiles/ThreeVertexMap.dot"
-    override val assets = "assetsJsons/example_assets.json"
-    override val scenario = "scenarioJsons/example_scenario.json"
-    override val maxTicks = 1
+    override val assets = "assetsJsons/assetsVehUnav.json"
+    override val scenario = "scenarioJsons/assetsVehUnavScen.json"
+    override val maxTicks = 100
 
     override suspend fun run() {
-        // everything is parsed and validated
-        assertNextLine("Initialization Info: example_map.dot successfully parsed and validated")
-        assertNextLine("Initialization Info: example_assets.json successfully parsed and validated")
-        assertNextLine("Initialization Info: example_scenario.json successfully parsed and validated")
-        // The Simulation starts with tick 0
+        assertNextLine("Initialization Info: ThreeVertexMap.dot successfully parsed and validated")
+        assertNextLine("Initialization Info: assetsVehUnav.json successfully parsed and validated")
+        assertNextLine("Initialization Info: assetsVehUnavScen.json successfully parsed and validated")
         assertNextLine("Simulation starts")
         assertNextLine("Simulation Tick: 0")
-        // The Simulation should end
+        assertNextLine("Simulation Tick: 1")
+        assertNextLine("Emergency Assignment: 0 assigned to 1")
+        assertNextLine("Asset Allocation: 1 assigned to 0; 1 ticks to arrive.")
+        assertNextLine("Simulation Tick: 2")
+        assertNextLine("Asset Arrival: 1 arrived at 2.")
+        assertNextLine("Emergency Handling Start: 0 handling started.")
+        assertNextLine("Simulation Tick: 3")
+        assertNextLine("Emergency Resolved: 0 resolved.")
+        assertNextLine("Simulation Tick: 4")
+        assertNextLine("Simulation Tick: 5")
+        assertNextLine("Simulation Tick: 6")
+        assertNextLine("Event Triggered: 0 triggered.")
+        assertNextLine("Simulation Tick: 7")
+        assertNextLine("Emergency Assignment: 1 assigned to 1")
+        assertNextLine("Asset Allocation: 10 assigned to 1; 1 ticks to arrive.")
+        assertNextLine("Simulation Tick: 8")
+        assertNextLine("Asset Arrival: 1 arrived at 2.")
+        assertNextLine("Emergency Handling Start: 0 handling started.")
+
+
+
+
         assertNextLine("Simulation End")
-        // Statistics
         assertNextLine("Simulation Statistics: 0 assets rerouted.")
-        assertNextLine("Simulation Statistics: 0 received emergencies.")
+        assertNextLine("Simulation Statistics: 2 received emergencies.")
         assertNextLine("Simulation Statistics: 0 ongoing emergencies.")
         assertNextLine("Simulation Statistics: 0 failed emergencies.")
-        assertNextLine("Simulation Statistics: 0 resolved emergencies.")
-        // end of file is reached
+        assertNextLine("Simulation Statistics: 2 resolved emergencies.")
         assertEnd()
     }
 }
